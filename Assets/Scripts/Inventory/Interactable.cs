a3f4a4de0e5fccba4def08d8651d2b42ea977486
mcYiu <mcyiu@tlu.ee>
2023-05-29 17:35:00 +0300

    Change the Size of BG images && ItemPickUp

--- Assets/Scripts/Inventory/Interactable.cs
+++ Assets/Scripts/Inventory/Interactable.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class Interactable : MonoBehaviour
 {
-
     public virtual void Interact()
     {
        
--- Assets/Scripts/Inventory/ItemPickup.cs
+++ Assets/Scripts/Inventory/ItemPickup.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 public class ItemPickup : MonoBehaviour
 {
     public Item item;
+    bool isCollectable = false;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isCollectable = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isCollectable = false;
+    }
 
     void OnMouseDown()
     {
-        if (InventoryManager.instance.Add(item))
+        if (isCollectable && InventoryManager.instance.Add(item))
         {
             gameObject.SetActive(false);
             GameManager.instance.isHammerPickedUp = true;
